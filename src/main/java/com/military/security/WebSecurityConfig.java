b5505f04ab4585334ded502c382a74c79b303c69
chienhn <chienhn@mic.vn>
2026-07-17 10:28:10 +0700

    config cors

--- src/main/java/com/military/security/WebSecurityConfig.java
+++ src/main/java/com/military/security/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -83,8 +84,9 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
     http.csrf(csrf -> csrf.disable())
         .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(auth -> 
-          auth.requestMatchers("/api/auth/signin").permitAll()
+        .authorizeHttpRequests(auth ->
+          auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+              .requestMatchers("/api/auth/signin").permitAll()
               .requestMatchers("/api/common/images/**").permitAll()
               .requestMatchers("/swagger-ui.html").permitAll()
               .requestMatchers("/swagger-ui/**").permitAll()
